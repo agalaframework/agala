49f9bc183cd637c1af16d804215b8b190e8f39be
Dmitry Rubinstein <rubinsteindb@gmail.com>
2016-10-12 16:56:50 +0300

    Fixing version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Agala.Mixfile do
 
   def project do
     [app: :agala,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
