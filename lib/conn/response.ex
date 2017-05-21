21c97c768397c56f6894d843c2463804e031d2a7
Dmitry Rubinstein <rubinsteindb@gmail.com>
2017-05-21 16:53:31 +0000

    Backing Agala.Response

--- lib/conn/response.ex
+++ lib/conn/response.ex
@@ -1,3 +1,19 @@
-defmodule Agala.Conn.Response do
-  defstruct [:provider, :method, :payload]
+defmodule Agala.Response do
+  defstruct [
+    name: nil,
+    method: :noaction,
+    url: nil,
+    body: nil,
+    headers: nil,
+    http_opts: nil
+  ]
+
+  @type t :: %Agala.Response{
+    name: atom,
+    method: :noaction | :get | :post | :put | :delete,
+    url: String.t,
+    body: String.t,
+    headers: [{String.t, String.t}],
+    http_opts: Keyword.t
+  }
 end
