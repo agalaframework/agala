47ace0d70dca2cdf9767c4e680c751ca5eb59c2d
Dmitry Rubinstein <rubinsteindb@gmail.com>
2017-05-30 07:38:17 +0000

    BUGFIX: Fixing body encode error!

--- lib/providers/telegram/responser.ex
+++ lib/providers/telegram/responser.ex
@@ -2,7 +2,7 @@ defmodule Agala.Provider.Telegram.Responser do
   defmacro __using__(_) do
     quote location: :keep do
       defp create_body(conn = %Agala.Conn{response: %{payload: %{body: body}}}) when is_bitstring(body), do: body
-      defp create_body(conn = %Agala.Conn{response: %{payload: %{body: body}}}) when is_map(body), do: body
+      defp create_body(conn = %Agala.Conn{response: %{payload: %{body: body}}}) when is_map(body), do: body |> Poison.encode!
       defp create_body(conn), do: ""
 
       defp create_url(conn = %Agala.Conn{response: %{payload: %{url: url}}}, bot_params) when is_function(url) do
@@ -14,8 +14,8 @@ defmodule Agala.Provider.Telegram.Responser do
           conn.response.method,
           create_url(conn, bot_params),
           create_body(conn),
-          Map.get(conn.response, :headers, []),
-          Map.get(conn.response, :http_opts) || Map.get(bot_params.private, :http_opts) || []
+          Map.get(conn.response.payload, :headers, []),
+          Map.get(conn.response.payload, :http_opts) || Map.get(bot_params.private, :http_opts) || []
         )
       end
     end
