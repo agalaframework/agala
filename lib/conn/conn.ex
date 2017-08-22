2bb6eb64cc6a7dfa604ffe74f5a73f85ed515199
Dmitry Rubinstein <rubinsteindb@gmail.com>
2017-08-22 13:23:37 +0300

    Adding Access impl to Conn

--- lib/conn/conn.ex
+++ lib/conn/conn.ex
@@ -24,6 +24,26 @@ defmodule Agala.Conn do
     responser_name: String.t | Atom
   }
 
+  @behaviour Access
+  @doc false
+  def fetch(bot_params, key) do
+    Map.fetch(bot_params, key)
+  end
+
+  @doc false
+  def get(structure, key, default \\ nil) do
+    Map.get(structure, key, default)
+  end
+
+  @doc false
+  def get_and_update(term, key, list) do
+    Map.get_and_update(term, key, list)
+  end
+
+  @doc false
+  def pop(term, key) do
+    {get(term, key), term}
+  end
 
   @doc """
   Halts the Agala.Chain pipeline by preventing further plugs downstream from being
